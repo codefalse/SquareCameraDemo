1955d875ae220edd070c7998459708c87ca96b46
moocher <songxiang_feng@163.com>
2015-08-02 23:58:05 +0800

    square
    square

--- squarecameralibrary/src/main/java/com/moocher/squarecameralibrary/CameraActivity.java
+++ squarecameralibrary/src/main/java/com/moocher/squarecameralibrary/CameraActivity.java
@@ -14,6 +14,6 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+
     }
 }
-
